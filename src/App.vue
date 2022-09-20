d4893329488c6c8e98b16fd090a40006df5345f4
Yunus Emre Topcu <yunusemre_topcu@hotmail.com>
2022-09-20 14:09:24 +0300

    homework completed

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,46 @@
 <script setup>
-import BootcampLogo from "./assets/svg/BootcampLogo.vue"
-import HelloWorld from './components/HelloWorld.vue'
+import Students from "./components/Students.vue";
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+const filteredDatas = ref();
+
+async function getStudents() {
+	const students = await axios
+		.get("src/assets/data/students.json")
+		.then((student) => student.data);
+	return students;
+}
+
+function getGroupNames(data) {
+	const groups = data.map((student) => student.group);
+	const uniqNames = [...new Set(groups)];
+	return uniqNames;
+}
+
+function filterByAsistants(data) {
+	let asistants = data.filter((student) => student.type);
+	let students = data.filter((student) => !student.type);
+
+	asistants = asistants.map((asistant) => {
+		const filteredStudents = students.filter(
+			(student) => student.group === asistant.group
+		);
+		return {
+			group: asistant.group,
+			asistants: asistant.name,
+			students: filteredStudents,
+		};
+	});
+	return asistants;
+}
+onMounted(async () => {
+	const data = await getStudents();
+	getGroupNames(data);
+	filteredDatas.value = filterByAsistants(data);
+});
 </script>
 
 <template>
-	<div>
-		<BootcampLogo />
-		<br />
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
-	</div>
-	<HelloWorld msg="Vite + Vue" />
+	<Students :datas="filteredDatas" />
 </template>
-
-<style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-}
-
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/Students.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="container" v-for="data in datas" :key="data.name">
+		<div class="grouplist">
+			<h3 class="grouplist__group">
+				{{ data.group }}
+			</h3>
+
+			<hr class="line" />
+
+			<ul class="students" v-for="student in data.students" :key="student.id">
+				<li class="students__list">{{ student.name }}</li>
+			</ul>
+
+			<div class="asistant" :class="data.group.toLowerCase()">
+				{{ data.asistants }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+const props = defineProps(["datas"]);
+</script>
